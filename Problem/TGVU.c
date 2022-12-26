7a05d2c6929567eb4f8cba63a8b41e40fd66315f
N1ghtf4ll <tri345345@gmail.com>
2022-12-26 22:11:32 +0700

    TGVU

--- Problem/TGVU.c
+++ Problem/TGVU.c
@@ -4,7 +4,16 @@
 #include <stdio.h>
 int main()
 {
-
+   int n, i, j;
+   scanf("%d", &n);
+   for (i = 0; i < n; i++)
+   {
+      for (j = 0; j < n - i; j++)
+      {
+         printf("*");
+      }
+      printf("%c", '\n');
+   }
    return 0;
 }
 
