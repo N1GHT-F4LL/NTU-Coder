4bab1e83b9acd6b828a9ff363d36dccf3812baf2
N1ghtf4ll <tri345345@gmail.com>
2022-12-26 22:28:30 +0700

    Số fibonacy

---
+++ Problem/FIBO.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+   long long a[35] = {};
+   int n, i;
+   a[0] = 1;
+   a[1] = 1;
+   scanf("%d", &n);
+   for (i = 2; i <= 30; i++)
+   {
+      a[i] = a[i - 1] + a[i - 2];
+   }
+   for (i = 0; i < n; i++)
+   {
+      printf("%lld ", a[i]);
+   }
+   return 0;
+}
+
+// From N1ghtf4ll with luv
