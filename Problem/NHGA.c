2833b1308d06438a020c667cb244010dad789a79
N1ghtf4ll <tri345345@gmail.com>
2023-01-03 21:40:43 +0700

    NHGA - Nhà gần nhất

--- Problem/NHGA.c
+++ Problem/NHGA.c
@@ -3,6 +3,64 @@ int min(int a, int b)
 {
    return a < b ? a : b;
 }
+
+void merge(int arr[], int l, int m, int r)
+{
+   int i, j, k;
+   int n1 = m - l + 1;
+   int n2 = r - m;
+
+   int L[n1], R[n2];
+
+   for (i = 0; i < n1; i++)
+      L[i] = arr[l + i];
+   for (j = 0; j < n2; j++)
+      R[j] = arr[m + 1 + j];
+
+   i = 0;
+   j = 0;
+   k = l;
+   while (i < n1 && j < n2)
+   {
+      if (L[i] <= R[j])
+      {
+         arr[k] = L[i];
+         i++;
+      }
+      else
+      {
+         arr[k] = R[j];
+         j++;
+      }
+      k++;
+   }
+
+   while (i < n1)
+   {
+      arr[k] = L[i];
+      i++;
+      k++;
+   }
+
+   while (j < n2)
+   {
+      arr[k] = R[j];
+      j++;
+      k++;
+   }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+   if (l < r)
+   {
+      int m = l + (r - l) / 2;
+      mergeSort(arr, l, m);
+      mergeSort(arr, m + 1, r);
+      merge(arr, l, m, r);
+   }
+}
+
 int main()
 {
    int n, a[100001], lowest = 1000000000, temp;
@@ -11,18 +69,7 @@ int main()
    {
       scanf("%d", &a[i]);
    }
-   for (int i = 0; i < n - 1; i++)
-   {
-      for (int j = i + 1; j < n; j++)
-      {
-         if (a[i] > a[j])
-         {
-            temp = a[i];
-            a[i] = a[j];
-            a[j] = temp;
-         }
-      }
-   }
+   mergeSort(a, 0, n - 1);
    for (int i = 0; i < n - 1; i++)
    {
       lowest = min(lowest, a[i + 1] - a[i]);
